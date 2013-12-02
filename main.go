00f9ae04f1ae06dc1cf167139d4651f2b8c3fc9a
Lachie Cox <lachiec@gmail.com>
2013-12-02 21:17:14 +1100

    flesh out main package

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"flag"
-  "launchpad.net/goamz/s3"
-  "os"
-  "fmt"
+  //"launchpad.net/goamz/s3"
+  //"os"
   "log"
+  "dogestry/cli"
+  "dogestry/client"
 )
 
 
 func main() {
 	flag.Parse()
+
+  client,err := client.NewClient("unix:///var/run/docker.sock")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  cli.ParseCommands(client, flag.Args()...)
+
+  log.Println("ok, done")
 }
