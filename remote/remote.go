ce480f7df25a479fc6a614221951d845aad301bc
lachie <lachiec@gmail.com>
2013-12-03 11:30:02 +1100

    switch remote impl based on the url

--- remote/remote.go
+++ remote/remote.go
@@ -1,34 +1,66 @@
 package remote
 
 import (
-  "errors"
-  "fmt"
+	"errors"
+	"fmt"
 	"net/url"
 )
+
+type Remote interface {
+	Push() error
+}
+
+type RemoteSpec struct {
+	url       url.URL
+	image     string
+	imageRoot string
+}
 
 var (
 	// ErrInvalidRemote is returned when the remote is not a valid.
 	ErrInvalidRemote = errors.New("Invalid endpoint")
 )
 
-
-
 func Push(remote, image, imageRoot string) error {
-  remoteUrl,_ := parseRemoteDef(remote)
-  fmt.Println("url", remoteUrl)
-  return nil
+	remoteUrl, err := normaliseURL(remote)
+	if err != nil {
+		return err
+	}
+
+	spec := RemoteSpec{
+		url:       *remoteUrl,
+		image:     image,
+		imageRoot: imageRoot,
+	}
+
+	remoteImpl, err := findRemoteImpl(spec)
+	if err != nil {
+		return err
+	}
+
+	return remoteImpl.Push()
 }
 
+func findRemoteImpl(spec RemoteSpec) (Remote, error) {
+	switch spec.url.Scheme {
+	case "rsync":
+		return NewRsyncRemote(spec)
+	default:
+		return nil, fmt.Errorf("unknown remote type %s", spec.url.Scheme)
+	}
+}
 
-func parseRemoteDef(remoteUrl string) (*url.URL, error) {
-  u, err := url.Parse(remoteUrl)
+func normaliseURL(remoteUrl string) (*url.URL, error) {
+	u, err := url.Parse(remoteUrl)
 	if err != nil {
 		return nil, ErrInvalidRemote
 	}
 
+	if u.Scheme == "" {
+		u.Scheme = "rsync"
+	}
 
-  fmt.Println("sch", u.Scheme, u.Path)
+	fmt.Println("sch", u.Scheme, u.Path)
 
-
-  return u, nil
+	return u, nil
 }
