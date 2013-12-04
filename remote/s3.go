1a3e587cfcab5034160908e2a8b7721bddefd4bc
Lachie Cox <lachiec@gmail.com>
2013-12-04 18:20:32 +1100

    some more on s3

--- remote/s3.go
+++ remote/s3.go
@@ -1,16 +1,41 @@
 package remote
 
 import (
+	"launchpad.net/goamz/aws"
 	"launchpad.net/goamz/s3"
 )
 
 type S3Remote struct {
-	Bucket    string
+	Bucket    s3.Bucket
 	KeyPrefix string
 }
 
+var (
+  S3DefaultRegion = "us-west-2"
+)
+
 func NewS3Remote(url url.URL) (*S3Remote, error) {
-	return &S3Remote{}
+  auth, err := aws.EnvAuth()
+  if err != nil {
+    return &S3Remote{}, err
+  }
+
+  regionName := url.Query()["region"]
+  if regionName == "" {
+    regionName = S3DefaultRegion
+  }
+
+  // hrmm
+  auth, err := aws.EnvAuth()
+
+  s3 := s3.New(auth, aws.Regions[regionName])
+
+  bucket := s3.Bucket(url.Host)
+
+	return &S3Remote{
+    Bucket: bucket,
+    KeyPrefix: url.Path,
+  }, nil
 }
 
 func (remote *S3Remote) Desc() string {
