2640dd42a5e5dc95eb6b34f2d81a888ae99e4480
Lachie Cox <lachiec@gmail.com>
2013-12-16 12:57:31 +1100

    first tests!!

---
+++ remote/s3_responses_test.go
@@ -0,0 +1,45 @@
+package remote
+
+var GetListResultDump1 = `
+<?xml version="1.0" encoding="UTF-8"?>
+<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01">
+  <Name>quotes</Name>
+  <Prefix>N</Prefix>
+  <IsTruncated>false</IsTruncated>
+  <Contents>
+    <Key>prefix/Nelson</Key>
+    <LastModified>2006-01-01T12:00:00.000Z</LastModified>
+    <ETag>&quot;828ef3fdfa96f00ad9f27c383fc9ac7f&quot;</ETag>
+    <Size>5</Size>
+    <StorageClass>STANDARD</StorageClass>
+    <Owner>
+      <ID>bcaf161ca5fb16fd081034f</ID>
+      <DisplayName>webfile</DisplayName>
+     </Owner>
+  </Contents>
+  <Contents>
+    <Key>prefix/Nelson.sum</Key>
+    <LastModified>2006-01-01T12:00:00.000Z</LastModified>
+    <ETag>&quot;828ef3fdfa96f00ad9f27c383fc9ac7f&quot;</ETag>
+    <Size>5</Size>
+    <StorageClass>STANDARD</StorageClass>
+    <Owner>
+      <ID>bcaf161ca5fb16fd081034f</ID>
+      <DisplayName>webfile</DisplayName>
+     </Owner>
+  </Contents>
+  <Contents>
+    <Key>prefix/Neo</Key>
+    <LastModified>2006-01-01T12:00:00.000Z</LastModified>
+    <ETag>&quot;828ef3fdfa96f00ad9f27c383fc9ac7f&quot;</ETag>
+    <Size>4</Size>
+    <StorageClass>STANDARD</StorageClass>
+     <Owner>
+      <ID>bcaf1ffd86a5fb16fd081034f</ID>
+      <DisplayName>webfile</DisplayName>
+    </Owner>
+ </Contents>
+</ListBucketResult>
+`
+
+
---
+++ remote/s3_test.go
@@ -0,0 +1,101 @@
+package remote
+
+import (
+	//"bytes"
+	//"io/ioutil"
+	//"net/http"
+	"testing"
+	//"strings"
+	"time"
+
+	"github.com/lachie/goamz/aws"
+	"github.com/lachie/goamz/s3"
+	"github.com/lachie/goamz/testutil"
+	. "launchpad.net/gocheck"
+
+  "fmt"
+
+  "dogestry/config"
+)
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type S struct {
+	remote *S3Remote
+}
+
+var _ = Suite(&S{})
+
+var testServer = testutil.NewHTTPServer()
+
+var baseConfig = RemoteConfig{
+  Config: config.Config{
+    S3: config.S3Config{
+      Access_Key_Id: "abc",
+      Secret_Key: "123",
+    },
+  },
+}
+
+func (s *S) SetUpSuite(c *C) {
+	testServer.Start()
+
+  auth := aws.Auth{"abc", "123", ""}
+  client := s3.New(auth, aws.Region{Name: "faux-region-1", S3Endpoint: testServer.URL})
+
+  //remote,err := NewRemote("s3://bucket/prefix&region=faux-region-1", baseConfig)
+  //if err != nil {
+    //panic(err)
+  //}
+
+  //s.remote = remote.(*S3Remote)
+
+
+  s.remote = &S3Remote{
+    config: baseConfig,
+    BucketName: "bucket",
+    KeyPrefix:  "prefix",
+    client:     client,
+  }
+}
+
+func (s *S) TearDownSuite(c *C) {
+  s3.SetAttemptStrategy(nil)
+}
+
+func (s *S) SetUpTest(c *C) {
+  attempts := aws.AttemptStrategy{
+    Total: 300 * time.Millisecond,
+    Delay: 100 * time.Millisecond,
+  }
+  s3.SetAttemptStrategy(&attempts)
+}
+
+
+func (s *S) TestBucket(c *C) {
+  testServer.Response(200, nil, "content")
+
+	b := s.remote.getBucket()
+  c.Assert(b.Name, Equals, "bucket")
+}
+
+func (s *S) TestRepoKeys(c *C) {
+  nelsonSha := "123"
+
+  testServer.Response(200, nil, "content")
+  testServer.Response(200, nil, GetListResultDump1)
+  testServer.Response(200, nil, nelsonSha)
+
+	keys,err := s.remote.repoKeys("")
+	c.Assert(err, IsNil)
+
+	testServer.WaitRequest()
+
+  c.Assert(keys["Nelson"].key, Equals, "Nelson")
+  c.Assert(keys["Nelson"].sum, Equals, nelsonSha)
+
+  c.Assert(keys["Neo"].key, Equals, "Neo")
+  c.Assert(keys["Neo"].sum, Equals, "")
+}
