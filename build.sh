f75cb1c6375e5111ff8dbcdaf5e4201a5e9e91b4
Lachie Cox <lachiec@gmail.com>
2014-02-12 12:42:20 +1100

    use aws cli's IAM profile support

--- build.sh
+++ build.sh
@@ -8,21 +8,7 @@ $d build -t dogestry .
 id=$($d inspect dogestry | jq -r '.[0].container')
 $d cp $id:dogestry .
 
-md=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials//buildbox-staging)
-access_key_id=$(echo $md | jq -r .AccessKeyId)
-secret_key=$(echo $md | jq -r .SecretAccessKey)
-
-S3CFG=.s3cfg
-cat <<EOC > $S3CFG
-[default]
-aws_access_key=$access_key_id
-aws_secret_key=$secret_key
-region=us-west-2
-EOC
-trap EXIT "rm $S3CFG"
-
-chmod 0600 $S3CFG
-
 BUCKET=ops-data-oregon.blakedev.com
-AWS_CONFIG_FILE=$S3CFG aws s3 cp --acl public-read dogestry s3://$BUCKET/bin/dogestry
+REGION=us-west-2
+aws s3 cp --acl public-read --region $REGION dogestry s3://$BUCKET/bin/dogestry
 
