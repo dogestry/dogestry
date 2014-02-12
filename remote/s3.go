91d65a910c4739bcfcf4ced87ebb2516f351aa32
lachie <lachiec@gmail.com>
2014-02-13 09:40:51 +1100

    oops

--- remote/s3.go
+++ remote/s3.go
@@ -88,7 +88,7 @@ func getS3Auth(config RemoteConfig) (aws.Auth, error) {
 
 func (remote *S3Remote) Validate() error {
   bucket := remote.getBucket()
-  list,err := bucket.List(remote.KeyPrefix, "", "", 1)
+  _,err := bucket.List(remote.KeyPrefix, "", "", 1)
   if err != nil {
     return fmt.Errorf("%s unable to ping s3 bucket: %s", remote.Desc(), err)
   }
