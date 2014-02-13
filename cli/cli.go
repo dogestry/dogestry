8c0c86b0498140205c9c89b32928aa7a496abf8a
lachie <lachiec@gmail.com>
2014-02-13 14:36:04 +1100

    print some help

--- cli/cli.go
+++ cli/cli.go
@@ -115,9 +115,18 @@ func (cli *DogestryCli) CmdHelp(args ...string) error {
 			method("--help")
 			return nil
 		}
-	}
+  }
 
-	fmt.Println("you are beyond help", args)
+
+    help := fmt.Sprintf("Usage: dogestry [OPTIONS] COMMAND [arg...]\nAlternate registry and simple image storage for docker.\n\nCommands:\n")
+	for _, command := range [][]string{
+		{"pull", "Pull an image from a remote"},
+		{"push", "Push an image to a remote"},
+		{"remote", "Check a remote"},
+	} {
+		help += fmt.Sprintf("    %-10.10s%s\n", command[0], command[1])
+	}
+	fmt.Println("%s\n", help)
 	return nil
 }
 
