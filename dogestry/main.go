8b0a3ea13e0ad2d99d70383d087585a0e9e5fe4d
Tom Offermann <toffermann@newrelic.com>
2014-12-22 15:18:51 -0800

    Add 'hosts' command line flag.

--- dogestry/main.go
+++ dogestry/main.go
@@ -7,14 +7,30 @@ import (
 	"github.com/dogestry/dogestry/config"
 	"log"
 	"runtime"
+	"strings"
 )
+
+type hosts []string
+
+func (h *hosts) String() string {
+	return fmt.Sprintf("%v", *h)
+}
+
+func (h *hosts) Set(value string) error {
+	for _, host := range strings.Split(value, ",") {
+		*h = append(*h, host)
+	}
+	return nil
+}
 
 var flConfigFile string
 var flTempDir string
+var flHosts hosts
 
 func init() {
 	flag.StringVar(&flConfigFile, "config", "", "the dogestry config file (defaults to 'dogestry.cfg' in the current directory). Config is optional - if using s3 you can use env vars or signed URLs.")
 	flag.StringVar(&flTempDir, "tempdir", "", "an alternate tempdir to use")
+	flag.Var(&flHosts, "hosts", "a comma-separated list of hosts")
 }
 
 func main() {
