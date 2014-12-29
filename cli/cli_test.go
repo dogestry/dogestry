e4953935d7ca385dd9a5b91aa66a869ea220d560
Didip Kerabat <didip@newrelic.com>
2014-12-29 11:16:19 -0800

    Add a few basic tests to cli.go

---
+++ cli/cli_test.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"github.com/dogestry/dogestry/config"
+	"os"
+	"testing"
+)
+
+func TestNewDogestryCli(t *testing.T) {
+	cfg, err := config.NewConfig("")
+	if err != nil {
+		t.Fatalf("Creating config struct should work. Error: %v", err)
+	}
+
+	dogestryCli, err := NewDogestryCli(cfg)
+	if err != nil {
+		t.Fatalf("Creating dogestryCli struct should work. Error: %v", err)
+	}
+
+	if dogestryCli.Client == nil {
+		t.Fatal("dogestryCli.Client should never be nil.")
+	}
+}
+
+func TestCreateAndReturnTempDirAndCleanup(t *testing.T) {
+	cfg, _ := config.NewConfig("")
+	dogestryCli, _ := NewDogestryCli(cfg)
+
+	tmpDir := dogestryCli.CreateAndReturnTempDir()
+	if tmpDir == "" {
+		t.Fatalf("CreateAndReturnTempDir should always return path to tmp directory. tmpDir: %v", tmpDir)
+	}
+
+	dogestryCli.Cleanup()
+	if _, err := os.Stat(tmpDir); err == nil {
+		t.Fatalf("Cleanup() should remove tmp directory. tmpDir: %v", tmpDir)
+	}
+}
