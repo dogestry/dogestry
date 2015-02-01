9c9d9a781b9fe894878f26ddafd7fe8f2121ffd6
Tom Offermann <toffermann@newrelic.com>
2015-01-31 22:36:53 -0800

    Improve help message for `dogestry help remote`.

--- cli/remote.go
+++ cli/remote.go
@@ -5,17 +5,24 @@ import (
 	"github.com/dogestry/dogestry/remote"
 )
 
+const RemoteHelpMessage string = `  Arguments:
+    REMOTE       Name of REMOTE.
+
+  Examples:
+    dogestry remote s3://DockerBucket/Path/?region=us-east-1
+    dogestry remote /path/to/images`
+
 func (cli *DogestryCli) CmdRemote(args ...string) error {
-	cmd := cli.Subcmd("remote", "REMOTE", "describes a remote")
-	if err := cmd.Parse(args); err != nil {
+	remoteFlags := cli.Subcmd("remote", "REMOTE", RemoteHelpMessage)
+	if err := remoteFlags.Parse(args); err != nil {
 		return nil
 	}
 
-	if len(cmd.Args()) < 1 {
+	if len(remoteFlags.Args()) < 1 {
 		return fmt.Errorf("Error: REMOTE not specified")
 	}
 
-	remoteDef := cmd.Arg(0)
+	remoteDef := remoteFlags.Arg(0)
 
 	r, err := remote.NewRemote(remoteDef, cli.Config)
 	if err != nil {
