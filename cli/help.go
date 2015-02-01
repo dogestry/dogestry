49d3a1d94f69eedfed21c3eb9952e622f9fb9193
Tom Offermann <toffermann@newrelic.com>
2015-01-31 21:07:12 -0800

    Revise and reformat help message.

--- cli/help.go
+++ cli/help.go
@@ -17,18 +17,22 @@ func (cli *DogestryCli) CmdHelp(args ...string) error {
 
 	help := fmt.Sprintf(
 		`Usage: dogestry [OPTIONS] COMMAND [arg...]
-Alternate registry and simple image storage for docker.
-  Typical S3 Usage:
-     export AWS_ACCESS_KEY=ABC
-     export AWS_SECRET_KEY=DEF
-     export DOCKER_HOST=tcp://localhost:2375
-     dogestry push s3://<bucket name>/<path name>/?region=us-east-1 <image name>
-     dogestry pull s3://<bucket name>/<path name>/?region=us-east-1 <image name>
-     dogestry -pullhosts tcp://host-1:2375,tcp://host-2:2375 pull s3://<bucket name>/<path name>/ <image name>
+
   Commands:
-     pull     - Pull IMAGE from S3 and load it into docker. TAG defaults to 'latest'
-     push     - Push IMAGE to S3. TAG defaults to 'latest'
-     remote   - Check a remote
+     help        Print help message. Use help COMMAND for more specific help
+     pull        Pull IMAGE from remote and load it into docker
+     push        Push IMAGE from docker to remote
+     remote      Show info about remote
+     version     Print version
+
+  Options:
+     -config     Path to optional config file
+     -pullhosts  A comma-separated list of docker hosts where the image will be pulled
+
+  Typical S3 Usage:
+     dogestry push s3://<bucket name>/<path name>/?region=us-east-1 <image name>
+     dogestry pull s3://<bucket name>/<path name>/?region=us-west-1 <image name>
+     dogestry -pullhosts tcp://host-1:2375,tcp://host-2:2375 pull s3://<bucket name>/<path name>/ <image name>
 `)
 	fmt.Println(help)
 	return nil
