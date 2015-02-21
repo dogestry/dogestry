31c48336fcf7b8da26bde2f4b7e3f3767d504141
Tom Offermann <toffermann@newrelic.com>
2015-02-21 13:20:32 -0800

    Add help message for list.

--- cli/list.go
+++ cli/list.go
@@ -8,11 +8,31 @@ import (
 	"github.com/dogestry/dogestry/remote"
 )
 
+const ListHelpMessage string = `  List images on REMOTE.
+
+  Arguments:
+    REMOTE       Name of REMOTE.
+
+  Examples:
+    dogestry list s3://DockerBucket/Path/?region=us-east-1
+    dogestry list /path/to/images`
+
 func (cli *DogestryCli) CmdList(args ...string) error {
+	listFlags := cli.Subcmd("list", "REMOTE", ListHelpMessage)
+	if err := listFlags.Parse(args); err != nil {
+		return nil
+	}
+
+	if len(listFlags.Args()) < 1 {
+		fmt.Fprintln(cli.err, "Error: REMOTE not specified")
+		listFlags.Usage()
+		os.Exit(2)
+	}
+
+	remoteDef := listFlags.Arg(0)
+
 	w := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
 	defer w.Flush()
-
-	remoteDef := args[0]
 
 	r, err := remote.NewRemote(remoteDef, cli.Config)
 	if err != nil {
