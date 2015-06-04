7c1e825fa32c2aeea1e4bb8beb5f2dcd87fe577d
Didip Kerabat <didip@newrelic.com>
2015-06-04 13:54:10 -0700

    Read AWS_ACCESS_KEY and AWS_SECRET_KEY again.

--- config/config.go
+++ config/config.go
@@ -9,18 +9,23 @@ import (
 func NewConfig() (Config, error) {
 	c := Config{}
 	c.AWS.AccessKeyID = os.Getenv("AWS_ACCESS_KEY_ID")
+	if c.AWS.AccessKeyID == "" {
+		c.AWS.AccessKeyID = os.Getenv("AWS_ACCESS_KEY")
+	}
+
 	c.AWS.SecretAccessKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	if c.AWS.SecretAccessKey == "" {
+		c.AWS.SecretAccessKey = os.Getenv("AWS_SECRET_KEY")
+	}
+
 	c.Docker.Connection = os.Getenv("DOCKER_HOST")
 
 	if c.Docker.Connection == "" {
 		c.Docker.Connection = "unix:///var/run/docker.sock"
 	}
-	if "" != os.Getenv("DOCKER_HOST") {
-		c.Docker.Connection = os.Getenv("DOCKER_HOST")
-	}
 
 	if c.AWS.AccessKeyID == "" || c.AWS.SecretAccessKey == "" {
-		return c, errors.New("AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY are missing.")
+		return c, errors.New("AWS_ACCESS_KEY_ID/AWS_ACCESS_KEY or AWS_SECRET_ACCESS_KEY/AWS_SECRET_KEY are missing.")
 	}
 
 	return c, nil
