ae276502cba1ea199a8a7e0b62435feef786a967
Didip Kerabat <didip@newrelic.com>
2015-07-08 16:27:21 -0600

    Close all running goroutines when an error occur during push.

--- remote/s3.go
+++ remote/s3.go
@@ -126,21 +126,30 @@ func (remote *S3Remote) Push(image, imageRoot string) error {
 	defer close(putFileErrChan)
 
 	numGoroutines := 25
+	goroutineQuitChans := make([]chan bool, 25)
+	for i := 0; i < numGoroutines; i++ {
+		goroutineQuitChans[i] = make(chan bool)
+	}
 
 	println("Pushing files to S3 remote:")
 	for i := 0; i < numGoroutines; i++ {
-		go func() {
-			for putFile := range putFilesChan {
-				putFileErr := remote.putFile(putFile.KeyDef.fullPath, &putFile.KeyDef)
+		go func(i int) {
+			select {
+			case <-goroutineQuitChans[i]:
+				return
+			default:
+				for putFile := range putFilesChan {
+					putFileErr := remote.putFile(putFile.KeyDef.fullPath, &putFile.KeyDef)
 
-				if (putFileErr != nil) && ((putFileErr != io.EOF) && (!strings.Contains(putFileErr.Error(), "EOF"))) {
-					putFileErrChan <- putFileResult{putFile.Key, putFileErr}
-					return
+					if (putFileErr != nil) && ((putFileErr != io.EOF) && (!strings.Contains(putFileErr.Error(), "EOF"))) {
+						putFileErrChan <- putFileResult{putFile.Key, putFileErr}
+						return
+					}
+
+					putFileErrChan <- putFileResult{}
 				}
-
-				putFileErrChan <- putFileResult{}
 			}
-		}()
+		}(i)
 	}
 
 	for i := 0; i < len(keysToPush); i++ {
